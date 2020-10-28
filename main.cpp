e8aa3d1a38a3ffa5ed6664874168f1b1e956de6a
Simone Cosimo <simosx4@gmail.com>
2020-10-28 18:50:59 +0100

    Changed main to reflect optmial future use

--- main.cpp
+++ main.cpp
@@ -1,47 +1,31 @@
 #include "Vector3.h"
 #include "Color.h"
 #include "Image.h"
+#include "Scene.h"
+#include "Ray.h"
 #include <memory>
 
-#define IMG_WIDTH	3
-#define IMG_HEIGHT	2
+#define ASPECT_RATIO	16.0 / 9.0
+#define IMG_WIDTH		3
+#define IMG_HEIGHT		IMG_WIDTH / ASPECT_RATIO
 
 int main(int argc, char** argv) {
 
-	Color white(255, 255, 255);
-	Color black(0, 0, 0);
-	Color red(255, 0, 0);
-	Color green(0, 255, 0);
-	Color blue(0, 0, 255);
+	Image img("out.ppm", IMG_WIDTH, IMG_HEIGHT, 255);
 
-	Image avgImage("avg-test.ppm", IMG_WIDTH, IMG_HEIGHT, 255);
-	Image addImage("add-test.ppm", IMG_WIDTH, IMG_HEIGHT, 255);
+	Vector3 camera(0, 0, 0);
+	// Sphere sphere1(Vector3(0, 0, 0), 1, Color(255, 0, 0));
+	// Sphere sphere2(Vector3(0, 0, 0), 1.5, Color(0, 0, 255));
+	
+	Scene scene1(camera);
+	// scene1.addObject(sphere1);
+	
+	Scene scene2(camera);
+	scene2.setSkyboxType(skybox::solid);
+	scene2.setSkyboxColors(Color(170, 170, 170));
+	// scene2.addObject(sphere2);
 
-	// ADD mode
-	Color::setAddMode(mode::ADD);
-	Color yellow = red + green;
-	std::cout << "red + green = " << yellow << std::endl;
-	addImage.setPixel(0, 0, red);
-	addImage.setPixel(1, 0, green);
-	addImage.setPixel(2, 0, blue);
-	addImage.setPixel(0, 1, yellow);
-	addImage.setPixel(1, 1, white);
-	addImage.setPixel(2, 1, black);
-	addImage.fill();
-
-	// AVG mode
-	Color::setAddMode(mode::AVG);
-	Color mixed = red + green;
-	std::cout << "red + green = " << mixed << std::endl;
-	Color grey = white + black;
-	std::cout << "white + black = " << grey << std::endl;
-	avgImage.setPixel(0, 0, red);
-	avgImage.setPixel(1, 0, green);
-	avgImage.setPixel(2, 0, blue);
-	avgImage.setPixel(0, 1, mixed);
-	avgImage.setPixel(1, 1, grey);
-	avgImage.setPixel(2, 1, black);
-	avgImage.fill();
+	// img.render(scene1);
 
 	return 0;
 }
